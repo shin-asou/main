2016d6c4ab7555405c8a7bef783bb84d927e4d70
shin-asou <shin.asou0120@gmail.com>
2011-04-28 13:42:53 +0900

    Resolutions of Conflicts

--- Languages/Ruby/Tests/mspec/rubyspec/command_line/dash_upper_s_spec.rb
+++ Languages/Ruby/Tests/mspec/rubyspec/command_line/dash_upper_s_spec.rb
@@ -1,16 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "The -S command line option" do
-<<<<<<< HEAD
-  it 'finds the file on the path' do
-    this_dir = File.expand_path(File.dirname(__FILE__))
-    fixture_dir = File.expand_path(this_dir + '/../fixtures')
-
-    ENV['PATH'] = "#{fixture_dir};#{ENV['PATH']}"
-    dash_s = ruby_exe(nil, :options => '-S file.rb')
-
-    ENV['PATH'] = ENV['PATH'].split(';')[1..-1].join(';')
-=======
 
   def push_path(dir)
     ENV['PATH'] = "#{dir};#{ENV['PATH']}"
@@ -34,7 +24,7 @@ describe "The -S command line option" do
     with_path(fixture_dir) do
       dash_s = ruby_exe(nil, :options => '-S file.rb')
     end
->>>>>>> 51920c25aaf2a24e82055e0807f51a19a01d3a40
+
     expected = ruby_exe("#{fixture_dir}/file.rb")
     failed_dash_s = ruby_exe(nil, :args => "-S #{this_dir}/file.rb 2>&1")
 
@@ -47,8 +37,6 @@ describe "The -S command line option" do
     normal = ruby_exe(nil, :args => 'does/not/exist.rb 2>&1')
     dash_s.should == normal
   end
-<<<<<<< HEAD
-=======
 
   it 'finds the first file on the path' do
     this_dir = File.expand_path(File.dirname(__FILE__))
@@ -70,5 +58,5 @@ describe "The -S command line option" do
       actual.should == File.expand_path(expected[i] + '/file.rb')
     end
   end
->>>>>>> 51920c25aaf2a24e82055e0807f51a19a01d3a40
+
 end
